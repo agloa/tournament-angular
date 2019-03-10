71ff7edcc2843291f198b90a6475f35179ad52bd
Mike Steigerwald <steiger@umich.edu>
2019-03-10 09:30:22 -0500

    remove unnecessary query object

--- app/core/individual/individual.service.js
+++ app/core/individual/individual.service.js
@@ -1,15 +1,10 @@
 'use strict';
 
-angular.
-  module('core.individual').
-  factory('Individual', ['$resource',
-    function($resource) {
-      return $resource('people/:personId.json', {}, {
-        query: {
-          method: 'GET',
-          params: {personId: 'people'},
-          isArray: true
-        }
-      });
+// Register a service factory, which will be called to return the service instance
+angular.module('core.individual').factory('Individual',  // name of the instance
+    ['$resource', // name of the The injectable $getFn
+    function($resource) { // The injectable $getFn
+      // call $resource service, provided by the ngResource module
+      return $resource('people/people.json'); // A parameterized URL template with parameters prefixed by :
     }
   ]);
