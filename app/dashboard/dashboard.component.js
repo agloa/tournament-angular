7f6968100d3ad44a4ca9ae75c608489493edeffa
Mike Steigerwald <steiger@umich.edu>
2019-03-24 15:36:27 -0500

    Improve readability

--- app/dashboard/dashboard.component.js
+++ app/dashboard/dashboard.component.js
@@ -1,20 +1,21 @@
 angular.module('tournament.dashboard').component('dashboard', {
   templateUrl: 'dashboard/dashboard.template.html',
-  bindings: {data: '<'},
   controller: ['$routeParams', 'User',
     function DashboardController($routeParams, User) {
       var self = this;
       self.personArrowClass = "arrowRight";
-      self.userId = 1003; // TODO: logged in user service
+      self.userId = 1003; // TODO: logged in user from session
       
+      // e.g., dashboard/43
       if ($routeParams.userId !== undefined) self.userId = $routeParams.userId;
 
-      self.includePersonForm = (self.userId !== undefined);
+      // Don't include 'child' form until user requests it.
+      self.includePersonForm = false;
 
       // User service returns additional data for userId
       self.user = User.get({userId:self.userId});
 
-      self.togglePerson = function togglePerson() {
+      self.togglePersonVisibility = function togglePersonVisibility() {
         this.includePersonForm = !this.includePersonForm;
         if (this.includePersonForm) {
           this.personArrowClass = "arrowDown";
